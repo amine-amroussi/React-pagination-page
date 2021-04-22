df1570664a63b5c61c139228bc321530c5162c62
Amine Amroussi <amineamro500@gmail.com>
2021-04-22 03:02:31 +0100

    React app pagination page

---
+++ src/Follower.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Follower = ({avatar_url , html_url , login}) => {
+  return <article className="card" >
+    <img src={avatar_url} alt={login} />
+    <h4> {login} </h4>
+    <a className="btn" href={html_url} target="_blanck" >
+      visit profile
+    </a>
+  </article>
+}
+
+export default Follower
